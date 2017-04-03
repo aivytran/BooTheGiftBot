41f5e5c7ef384ac26dd009b8dc68548b33bcefb4
aivytran <aivytran@berkeley.edu>
2017-04-02 18:46:27 -0700

    styled the policy page

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,16 +3,21 @@ import { Link, hashHistory} from 'react-router';
 
 const App = ({ children }) => (
   <div id="master-view">
-    <h1>Owlie The Gift Bot</h1>
-
-    <div className="fb-messengermessageus"
-      data-messenger_app_id="223589748119371"
-      data-page_id="1816355725292778"
-      data-color="blue"
-      data-size="standard" >test
+    <div className="privacy-header">
+      <h1>Owlie The Gift Bot</h1>
+      <img src={window.main_logo} alt="logo"/>
     </div>
 
-    <div>
+    <div className="facebook-button">
+      <div className="fb-messengermessageus"
+        data-messenger_app_id="223589748119371"
+        data-page_id="1816355725292778"
+        data-color="blue"
+        data-size="standard" >
+      </div>
+    </div>
+
+    <div className="policy-link">
       <Link onClick={() => hashHistory.push('/privacy')}>Privacy Policy</Link>
     </div>
   </div>
